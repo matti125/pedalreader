809e7ba4a3368cf2963cd2127190e3fee61ddb77
Matti Suuronen <matti.suuronen@iki.fi>
2015-01-26 14:05:55 +0000

    modified:   pedalread-pyusb.py made a graphical view of numbers read for debugging

--- pedalread-pyusb.py
+++ pedalread-pyusb.py
@@ -11,15 +11,15 @@ assert teensy is not None
 if pedals.is_kernel_driver_active(0):
 	print "detaching kernel driver"
 	pedals.detach_kernel_driver(0)
-	usb.util.claim_interface(pedals, intf_pedals)
 if teensy.is_kernel_driver_active(0):
 	print "detaching kernel driver"
 	teensy.detach_kernel_driver(0)
-	usb.util.claim_interface(teensy, intf_teensy)
 
 #dev.set_configuration()
 cfg = pedals.get_active_configuration()
 intf_pedals = cfg[(0,0)]
+usb.util.claim_interface(pedals, intf_pedals)
+
 ep_pedals_in = usb.util.find_descriptor(
     intf_pedals,
     # match the first IN endpoint
@@ -31,6 +31,7 @@ assert ep_pedals_in is not None
 
 cfg = teensy.get_active_configuration()
 intf_teensy = cfg[(0,0)]
+usb.util.claim_interface(teensy, intf_teensy)
 ep_teensy_in = usb.util.find_descriptor(
     intf_teensy,
     # match the first OUT endpoint
@@ -56,12 +57,11 @@ count=1
 t_gotdata=0
 t_min=5000
 
-
+d_wrote_max=0
 while (1):
    try:
-	pdata = pedals.read(ep_pedals_in.bEndpointAddress, ep_pedals_in.wMaxPacketSize, timeout=100)
+	pdata = pedals.read(ep_pedals_in.bEndpointAddress, ep_pedals_in.wMaxPacketSize, timeout=100000)
 	t=time.time();
-	print "gotdata"
 	t_now=time.time()
 	t_since_last_pedal=t_now-t_gotdata
 	t_min=min(t_min,t_since_last_pedal)
@@ -75,12 +75,17 @@ while (1):
 	temp[4]=(brake >> 8) & 255
 	temp[5]=clutch & 255
 	temp[6]=(clutch >> 8) & 255
-#	teensy.write(ep_teensy_out,temp)
-#	t_wrote=time.time()
+
 	
-#        data=[]
-	o=(acceleration << 4)* "*"
-	print o, acceleration
+	teensy.clear_halt(ep_teensy_out)
+	nw=teensy.write(ep_teensy_out,temp,timeout=2000)
+	
+	d_wrote=time.time()-t_now
+	d_wrote_max=max(d_wrote_max, d_wrote)
+	
+	
+	o=(acceleration >> 4)* "*"
+	print o, acceleration, " ",int(d_wrote*1000), " ", int(d_wrote_max*1000), "n",nw
 # 	print "a ", acceleration, \
 # 		"b ", brake, \
 # 		"c ", clutch, \
